2efffbead60c1ff8cc2e7c4827ac49df1218c2a1
TheNortheWind <143673137+TheNortheWind@users.noreply.github.com>
2024-03-01 16:39:27 -0800

    Added my first slint test
    Animations and windows galore!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ slint_playground/main.rs
@@ -0,0 +1,46 @@
+fn main() {    
+    slint::slint! {
+        export component HelloWorld inherits Window {
+            width: 600px;
+            height: 600px;
+            background: #639DDF;
+            Text {
+                text: "Hello World";
+                color: green;
+            }
+        }
+    }
+    HelloWorld::new().unwrap().run().unwrap();
+
+    const Z: u32 = 3;
+
+    if Z == 3 {
+        slint::slint! {
+            export component HelloWorld inherits Window {
+                preferred-width: 100px;
+                preferred-height: 100px;
+                background: area.pressed ? blue : yellow;
+                animate background {
+                    duration: 1000ms;
+                    iteration-count: -1;
+                    easing: ease;
+                }
+                area := TouchArea {}
+             }
+        }
+        HelloWorld::new().unwrap().run().unwrap();
+    }
+
+    if Z != 3 {
+        slint::slint! {
+            export component HelloWorld {
+                Text {
+                    text: "ghjiokjygudshisdjsidjskdjsjdskajlksjd;saojskd;sdsdsadsdsa'd''sadsa";
+                    color: blue;
+                }
+            }
+        }
+        HelloWorld::new().unwrap().run().unwrap();
+    }
+}
+
