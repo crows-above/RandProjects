07a2cff0bea93efbd09a1bab8f4f6331cdf4323c
TheNortheWind <143673137+TheNortheWind@users.noreply.github.com>
2024-07-16 17:54:33 -0700

    Update main.py
    Updated main.py to proto6

--- platformer/src/main.py
+++ platformer/src/main.py
@@ -14,17 +14,59 @@ pygame.mixer.music.load("/home/hedron/Code/pythonities/games/platformer/assets/r
 pygame.mixer.music.play(-1,0.0)
 
 # Game objects
-
-player = pygame.draw.rect(screen,(255,153,173),pygame.Rect(30,30,30,30))
-
+posx = 50
+posy = 50
+sizex = 30
+sizey = 30
 
 while True:
+    # quit handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     
+    keys = pygame.key.get_pressed() #key handler
+
+    #move handler
+    if keys[pygame.K_LEFT]:
+        posx -= 5
+    if keys[pygame.K_RIGHT]:
+        posx += 5
+    if keys[pygame.K_DOWN]:
+        posy += 5
+    if keys[pygame.K_UP]:
+        posy -= 5
+
+    #resize handler
+    if keys[pygame.K_a]:
+        sizex += 5
+    if keys[pygame.K_d]:
+        sizex -= 5
+    if keys[pygame.K_s]:
+        sizey += 5
+    if keys[pygame.K_c]:
+        sizey -= 5
+    
+    if sizex <= 0:
+        sizex = 5
+    if sizey <= 0:
+        sizey = 5
+
+    #containment facility
+    if posx > 600:
+        posx = 0
+    if posx < 0:
+        posx = 600
+    if posy > 600:
+        posy = 0
+    if posy < 0:
+        posy = 600
+    
+    screen.fill(bg_color)
     clock.tick(30)
+    pygame.draw.rect(screen,(255,153,173),pygame.Rect(posx,posy,sizex,sizey))
     pygame.display.update()
 
-# proto4:
-# Musicaa^^
+# proto6:
+# This world has become a prison + resizement capaibilites^^
+# Controls: Arrow keys to move | A to grow larger (x), D to grow smaller (x), S to grow larger (y), C to grow smaller (y)
