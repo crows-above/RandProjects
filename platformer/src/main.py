8a45523f9fdeafa5bfacae7ccd1a7e59130142bf
TheNortheWind <143673137+TheNortheWind@users.noreply.github.com>
2024-07-16 21:00:48 -0700

    replaced main.py with version 9
    main.py == proto9.py

--- platformer/src/main.py
+++ platformer/src/main.py
@@ -9,15 +9,20 @@ screen = pygame.display.set_mode(size)
 bg_color = (0, 0, 0)
 screen.fill(bg_color) 
 clock = pygame.time.Clock()
+grav_timer = 0 # for acceleration
+keytimer = [0,0] # for acceleration
 
-pygame.mixer.music.load("/home/hedron/Code/pythonities/games/platformer/assets/reflected-light.mp3")
+pygame.mixer.music.load("/home/hedron/Music/ascension.mp3") # !!!!!! For the github viewers, change this path !!!!!!!
 pygame.mixer.music.play(-1,0.0)
 
 # Game objects
-posx = 50
-posy = 50
-sizex = 30
-sizey = 30
+pos = [50,50]
+shade_change = [0,0,0] #x, y, switchx
+player_size = [30, 30]
+ground = pygame.Rect(0, 570, 600, 30)
+player = pygame.Rect(*pos, *player_size)
+shade_color = (255, 193, 213)
+shadea = pygame.Rect(pos[0] + shade_change[0], pos[1] - shade_change[1], *player_size)
 
 while True:
     # quit handler
@@ -28,45 +33,64 @@ while True:
     keys = pygame.key.get_pressed() #key handler
 
     #move handler
-    if keys[pygame.K_LEFT]:
-        posx -= 5
-    if keys[pygame.K_RIGHT]:
-        posx += 5
-    if keys[pygame.K_DOWN]:
-        posy += 5
-    if keys[pygame.K_UP]:
-        posy -= 5
 
-    #resize handler
-    if keys[pygame.K_a]:
-        sizex += 5
-    if keys[pygame.K_d]:
-        sizex -= 5
-    if keys[pygame.K_s]:
-        sizey += 5
-    if keys[pygame.K_c]:
-        sizey -= 5
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        pos[0] -= 10 + (5 * (keytimer[0]/10))
+        keytimer[1] = 0
+        if keytimer[0] <= 50:    
+            keytimer[0] += 1
+        if shade_change[0] > -50:
+            shade_change[0] += 1
+        if shade_change[2] != 1:
+            shade_change[2] = 1
+            shade_change[0] = 0
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        pos[0] += 10 + (5 * (keytimer[1]/10))
+        keytimer[0] = 0
+        if keytimer[1] <= 50:
+            keytimer[1] += 1
+        if shade_change[0] < 50:
+            shade_change[0] -= 1
+        if shade_change[2] != 0:
+            shade_change[2] = 0
+            shade_change[0] = 0
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
+        pos[1] -= 20
     
-    if sizex <= 0:
-        sizex = 5
-    if sizey <= 0:
-        sizey = 5
+    if pos[1] < 540:
+        shade_change[1] -= 1
+    else:
+        shade_change[1] = 0
 
     #containment facility
-    if posx > 600:
-        posx = 0
-    if posx < 0:
-        posx = 600
-    if posy > 600:
-        posy = 0
-    if posy < 0:
-        posy = 600
+    grav_timer += clock.get_time()
+    if pos[1] == 540:
+        grav_timer = 0
+    if pos[1] != 540:
+       pos[1] += 5 * (grav_timer/100) # gravity
+    if pos[0] > 600:
+        pos[0] = 0
+    if pos[0] < 0:
+        pos[0] = 600
+    if pos[1] > 600:
+        pos[1] = 540
+    if pos[1] < 0:
+        pos[1] = 540
+
+    if player.colliderect(ground):
+        pos[1] = 540
     
     screen.fill(bg_color)
     clock.tick(30)
-    pygame.draw.rect(screen,(255,153,173),pygame.Rect(posx,posy,sizex,sizey))
+    pygame.draw.rect(screen,(73,0,130),ground)
+    player = pygame.Rect(*pos,*player_size)
+    pygame.draw.rect(screen,(255,153,173),player)
+    shadea = pygame.Rect(pos[0] + shade_change[0], pos[1] - shade_change[1], *player_size)
+    pygame.draw.rect(screen,(shade_color),shadea)
     pygame.display.update()
 
-# proto6:
-# This world has become a prison + resizement capaibilites^^
+# proto9:
+# Added a blur mechanic, and removed the resize mechanic, added support for wasd (without the s) 
 # Controls: Arrow keys to move | A to grow larger (x), D to grow smaller (x), S to grow larger (y), C to grow smaller (y)
+# Ideas: I'd like to imagine the shade that follows your character as your characters dead sibling whos helping you on your journey. Don't ask me why I'm giving pink little cubes a storyline, I don't even know myself
+# The game mechanics still feel pretty clunky :/
